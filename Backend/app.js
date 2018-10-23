2aeea491e72c026b1d80e1d236f044dd7d2b2ad4
Pallav Agarwal <agpallav@gmail.com>
2018-10-23 19:38:57 -0400

    Implementing Login Continued
    Finished implementing Part 8

--- Backend/app.js
+++ Backend/app.js
@@ -35,7 +35,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,15 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+import { BookComponent } from './book/book.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent,
+    data: { title: 'Book List' }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Login' }
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    data: { title: 'Sign Up' }
+  },
+  { path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    BookComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  BrowserModule,
+  FormsModule,
+  HttpClientModule,
+  //~~AppRoutingModule,
+  RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+  )
+],
   providers: [],
   bootstrap: [AppComponent]
 })
