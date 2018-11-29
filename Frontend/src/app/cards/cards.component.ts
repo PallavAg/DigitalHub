ab37eb544b681d91041ec7c29e048d32eac9f0e7
Pallav Agarwal <agpallav@gmail.com>
2018-11-29 16:39:06 -0500

    Details shows on UI

--- Frontend/src/app/cards/cards.component.ts
+++ Frontend/src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-cards',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+
+  @Input() item:any;
 
   constructor() { }
 
