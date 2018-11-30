9237364c35708286908d9ecc37638558f8d04513
Pallav Agarwal <agpallav@gmail.com>
2018-11-29 19:58:24 -0500

    Automatically reloads the page

--- Frontend/src/app/columncomponent/columncomponent.component.ts
+++ Frontend/src/app/columncomponent/columncomponent.component.ts
@@ -8,19 +8,29 @@ import { MediaService } from '../media.service';
 })
 export class ColumncomponentComponent implements OnInit {
 
-  @Input() title:string;
+  @Input() title: string;
   items: any[] = [];
-  
+  @Input() parentSubject: any;
 
-  constructor(private mediaService: MediaService)  { }
+
+  constructor(private mediaService: MediaService) { }
 
   ngOnInit() {
-    this.mediaService.getLinks().subscribe(data => { 
-      this.items = data; 
+    this.loadData();
+    this.parentSubject.subscribe(event => {
+      this.loadData();
+    })
+  }
+
+  loadData() {
+    this.mediaService.getLinks().subscribe(data => {
+      this.items = data;
       console.log(data);
       let title = this.title;
-      this.items = this.items.filter(function (item) {return item.mediaType == title})
-    }, err => {window.alert(err);} );
+      this.items = this.items.filter(function (item) { return item.mediaType == title })
+    }, err => { window.alert(err); });
   }
+
+
 
 }
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MediaService } from 'src/app/media.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -17,22 +18,23 @@ export class HomeComponent implements OnInit {
   isExpired: String;
   userId: String;
   items: any[] = [];
-  
+  @Output() reloadData = new EventEmitter<any>();
+  parentSubject: Subject<any> = new Subject();
 
   constructor(private mediaService: MediaService) {
-   const tokenKey = localStorage.getItem("jwtToken");
-   const decodedToken = this.helper.decodeToken(tokenKey);
-   this.userId = decodedToken._id;
-   const expirationDate = this.helper.getTokenExpirationDate(tokenKey);
-   const isExpired = this.helper.isTokenExpired(tokenKey);
-   console.log("*****");
-   console.log(decodedToken);
+    const tokenKey = localStorage.getItem("jwtToken");
+    const decodedToken = this.helper.decodeToken(tokenKey);
+    this.userId = decodedToken._id;
+    const expirationDate = this.helper.getTokenExpirationDate(tokenKey);
+    const isExpired = this.helper.isTokenExpired(tokenKey);
+    console.log("*****");
+    console.log(decodedToken);
   }
 
-  sendToBack(){
+  sendToBack() {
     let media = { url: this.link, userId: this.userId } //JSON Object
 
-    this.mediaService.addLinks(media).subscribe(data => { window.alert("Successfully added your link");  }, err => {window.alert(err);} );
+    this.mediaService.addLinks(media).subscribe(data => { window.alert("Successfully added your link"); this.parentSubject.next('some value'); }, err => { window.alert(err); });
 
   }
 
